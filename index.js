bf20817cc0da96d9f7cd44b129fdc1102eafd769
Arthur Cabral <63428806+CabralArthur@users.noreply.github.com>
2024-08-28 01:06:06 -0300

    ft: inserting an interface and function types

--- index.js
+++ index.js
@@ -3,5 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = require("axios");
 var url = 'https://jsonplaceholder.typicode.com/posts/1';
 axios_1.default.get(url).then(function (response) {
-    console.log(response.data, 'response.data');
+    var post = response.data;
+    var id = post.id;
+    var title = post.title;
+    logPost(id, title);
 });
+var logPost = function (id, title) {
+    console.log("\n        The post with id: ".concat(id, "\n        has a title of: ").concat(title, "\n    "));
+};
